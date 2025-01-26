bda68d8e6d2cbda98803b8b5a15b53a737f64960
Andrelleroux <22568948@sun.ac.za>
2025-01-26 16:49:04 +0200

    Data

---
+++ Written_Report/.Rproj.user/88031215/sources/session-71bea566/AC8EAD70-contents
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(xts)
+library(portfolioBacktest)
+library(riskParityPortfolio)
+
+Risk_Parity <- function(data = LCL_Stocks_dat){
+
+
+
+}
---
+++ Written_Report/code/Data_Setup.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+
+Data_Setup <- function(){
+
+    LCL_Stocks_init <- read_rds("data/LCL_Stock_Returns.rds")
+    LCL_indice_init <- read_rds("data/LCL_Indices.rds")
+    Global_indice_init <- read_rds("data/Global_Indices.rds")
+    ASISA_rets_init <- read_rds("data/ASISA_Rets.rds")
+    Rebal_Days <- read_rds("data/Rebalance_days.rds")
+
+    jse_top_40 <- c(
+        "AGL", "AMS", "ANG", "APN", "BHP", "BID", "BTI", "BVT", "CFR", "CLS",
+        "CPI", "DSY", "EXX", "FSR", "GFI", "GRT", "IMP", "INL", "INP", "MCG",
+        "MNP", "MRP", "MTN", "NED", "NPN", "NRP", "OMU", "PRX", "REM", "RNI",
+        "SAP", "SBK", "SHP", "SLM", "SOL", "SSW", "VOD", "WHL"
+    )
+
+    LCL_Stocks_dat <- LCL_Stocks_init %>%
+        filter(str_sub(Tickers, 1, 3) %in% jse_top_40)
+
+}
