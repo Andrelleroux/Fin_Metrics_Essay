7ec4bfd1d64f1f0f05bb532db191cc4e19a4bca7
Andrelleroux <22568948@sun.ac.za>
2025-01-30 20:12:17 +0200

    Methodology

--- Written_Report/code/max_sharpe_ratio.R
+++ Written_Report/code/max_sharpe_ratio.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(quadprog)
 
-max_sharpe_ratio <- function(dataset, current_date) {
+max_sharpe_ratio <- function(dataset, current_date, Rfr = 0.006042) {
     filter_dataset <- dataset %>% filter(date >= current_date - years(5) & date <= current_date)
     stock_names <- colnames(filter_dataset)[-1]  # Exclude the 'date' column
     # Number of assets (stocks)
@@ -9,6 +9,8 @@ max_sharpe_ratio <- function(dataset, current_date) {
     # Calculate covariance and mean returns
     sigma <- cov(filter_dataset[-1])  # Exclude the date column
     mu <- colMeans(filter_dataset[-1])  # Exclude the date column
+
+    mu <- mu - Rfr
 
     # If the mean returns are too small, return zero weights
     if (all(mu <= 1e-8)) {
